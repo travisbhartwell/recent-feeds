aad8ec849a24c477f009ce9a5e0f3293e7055ee0
Travis B. Hartwell <nafai@travishartwell.net>
2009-02-15 14:54:10 -0700

    A few clean-ups

--- recent-feed.hs
+++ recent-feed.hs
@@ -1,9 +1,8 @@
 module Main where
 
 import Data.Binary
--- Eventually, I probably want to try to use bytestring-trie's
-import qualified Data.Map as Map 
-
+import qualified Data.Map as Map
+    
 import System.Directory
 import Text.Feed.Constructor
 import Text.Feed.Export
@@ -11,36 +10,37 @@ import Text.Feed.Import
 import Text.Feed.Query
 import Text.Feed.Types
 import Text.RSS.Syntax (rssItemComments)
-import Text.XML.Light 
-    
+import Text.XML.Light
+
 main :: IO ()
 main = do
   feed <- parseFeedFromFile "del.rss"
   items <- deDupWithSerializedMap $ feedItems feed
-  let feed = withFeedItems items feed
+  let feed = withFeedItems items feed  
   putStrLn $ ppTopElement $ xmlFeed feed
 
 type IdCountMap = Map.Map String Integer
 
 seenMapFile = "seenMap.ser"
+
 defaultValue = 0 :: Integer
 
 deDupWithSerializedMap :: [Item] -> IO [Item]
-deDupWithSerializedMap items = 
+deDupWithSerializedMap items =
     do
-      hasFile <- doesFileExist seenMapFile
-      seenMap <- if hasFile
-                 then
-                     decodeFile seenMapFile
-                 else
-                     return Map.empty
+      -- hasFile <- doesFileExist seenMapFile
+      -- seenMap <- if hasFile
+      --            then
+      --                decodeFile seenMapFile
+      --            else
+      --                return Map.empty
+      let seenMap = Map.empty
       let items = deDupItems items seenMap
-      encodeFile seenMapFile seenMap
+--      encodeFle seenMapFile seenMap
       return items
 
-                           
 deDupItems :: [Item] -> IdCountMap -> [Item]
-deDupItems items seenMap = deDupItems' items [] seenMap
+deDupItems items = deDupItems' items []
 
 deDupItems' :: [Item] -> [Item] -> IdCountMap -> [Item]
 deDupItems' (item:items) keptItems seenMap =
@@ -49,11 +49,11 @@ deDupItems' (item:items) keptItems seenMap =
       Just anId -> deDupItems' items itemsToKeep (Map.insert anId (count + 1) seenMap)
           where
             count = Map.findWithDefault defaultValue anId seenMap
-            itemsToKeep = if count > 0 then keptItems else keptItems ++ [item] 
+            itemsToKeep = traceShow count $ if count > 0 then keptItems else keptItems ++ [item]
 
-deDupItems' [] keptItems _ =  keptItems
-    
-          
+deDupItems' [] keptItems seenMap =  keptItems
+
+
 getDeliciousUrlId :: Item -> Maybe String
 getDeliciousUrlId (RSSItem item) = getDelIdFromUrl `fmap` rssItemComments item
 
